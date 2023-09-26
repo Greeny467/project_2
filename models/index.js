9bf96dfb04bf10d25af093085516918f2928e283
Dawson Claybrook <joystickclay@gmail.com>
2023-09-25 19:47:41 -0600

    Added routes for pageRoutes.js

--- models/index.js
+++ models/index.js
@@ -7,30 +7,36 @@ const Request = require('./request');
 // Each chat belongs to two users
 Chat.belongsTo(User, {
     foreignKey: 'user1_id',
+    as: 'user1',
     onDelete: 'CASCADE'
 });
 Chat.belongsTo(User, {
     foreignKey: 'user2_id',
+    as: 'user2',
     onDelete: 'CASCADE'
 })
 
 //  Each message belongs to two users, author and recipient
 Message.belongsTo(User, {
     foreignKey: 'author_id',
+    as: 'author',
     onDelete: 'CASCADE'
 });
 Message.belongsTo(User, {
     foreignKey: 'recipient_id',
+    as: 'recipient',
     onDelete: 'CASCADE'
 });
 
 // Each request belongs to two users, author and recipient
 Request.belongsTo(User, {
     foreignKey: 'author_id',
+    as: 'author',
     onDelete: 'CASCADE'
 });
 Request.belongsTo(User, {
     foreignKey: 'recipient_id',
+    as: 'recipient',
     onDelete: 'CASCADE'
 });
 
